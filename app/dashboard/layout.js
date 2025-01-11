c40a1e9827d902420ec7877bee14ec9667bf4f26
Naman Saini <namansaini464@gmail.com>
2025-01-11 13:24:11 +0530

    Day 5 part 2: completed the /dashboard/profile ui

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,22 +1,42 @@
-"use client"
-import React from 'react'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation';
-const layout = ({children}) => {
-    const pathname = usePathname();
+"use client";
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+const layout = ({ children }) => {
+  const pathname = usePathname();
   return (
     <div>
-        <div className=' flex text-center items-center h-8 bg-gray-800 text-white'>
-        <Link className='w-1/2 flex justify-center' href={'/dashboard/profile'}>
-        <div className={pathname == "/dashboard/profile" ? "font-bold border-b-2 border-blue-400" : ""}>Profile</div>
+      <div className=" flex text-center items-center h-8 bg-gray-800 text-white">
+        <Link className="w-1/2 flex justify-center" href={"/dashboard/profile"}>
+          <div
+            className={
+              pathname == "/dashboard/profile"
+                ? "font-bold border-b-2 border-blue-400"
+                : ""
+            }
+          >
+            Profile
+          </div>
         </Link>
-        <div className='w-px rounded-lg bg-blue-300 h-[60%]'></div>
-        <Link className='w-1/2 flex justify-center' href={'/dashboard/yourpage'}>
-        <div className={pathname == "/dashboard/yourpage" ? "font-bold border-b-2 border-blue-400" : ""}>Your Page</div>
+        <div className="w-px rounded-lg bg-blue-300 h-[60%]"></div>
+        <Link
+          className="w-1/2 flex justify-center"
+          href={"/dashboard/yourpage"}
+        >
+          <div
+            className={
+              pathname == "/dashboard/yourpage"
+                ? "font-bold border-b-2 border-blue-400"
+                : ""
+            }
+          >
+            Your Page
+          </div>
         </Link>
+      </div>
+      {children}
     </div>
-        {children}</div>
-  )
-}
+  );
+};
 
-export default layout
+export default layout;
--- app/dashboard/profile/page.js
+++ app/dashboard/profile/page.js
@@ -20,8 +20,41 @@ const Profile = () => {
 
   // If the session is available, render the profile page
   return (
-    <div>
-      <span>Hello {session.user.name}</span>
+    <div className="min-h-screen">
+    <div className="flex flex-col items-center gap-4 justify-center h-full">
+      <h1 className="text-3xl font-bold mt-10">Profile</h1>
+      <p className="text-xl font-bold">Welcome, {session.user.name}!</p>
+    </div>
+    <div className="flex flex-col mx-auto w-1/2 items-center gap-2">
+      <div className="flex flex-col ">
+        <label className="w-fit" htmlFor="Name">Name</label>
+      <input className="bg-[#414248] rounded-lg outline-none p-2 focus:border-blue-900 focus:border-2 h-8 w-96" type="text" id="Name" />
+        </div>
+      <div className="flex flex-col ">
+        <label className="w-fit" htmlFor="Email">Email</label>
+      <input className="bg-[#414248] rounded-lg outline-none p-2 focus:border-blue-900 focus:border-2 h-8 w-96" type="text" id="Email" />
+        </div>
+      <div className="flex flex-col ">
+        <label className="w-fit" htmlFor="Username">Username</label>
+      <input className="bg-[#414248] rounded-lg outline-none p-2 focus:border-blue-900 focus:border-2 h-8 w-96" type="text" id="Username" />
+        </div>
+      <div className="flex flex-col ">
+        <label className="w-fit" htmlFor="pfp">Profile Picture</label>
+      <input className="bg-[#414248] rounded-lg outline-none p-2 focus:border-blue-900 focus:border-2 h-8 w-96" type="text" id="pfp" />
+        </div>
+      <div className="flex flex-col ">
+        <label className="w-fit" htmlFor="cover">Cover Picture</label>
+      <input className="bg-[#414248] rounded-lg outline-none p-2 focus:border-blue-900 focus:border-2 h-8 w-96" type="text" id="cover" />
+        </div>
+      <div className="flex flex-col ">
+        <label className="w-fit" htmlFor="RazorPayId">RazorPay Id</label>
+      <input className="bg-[#414248] rounded-lg outline-none p-2 focus:border-blue-900 focus:border-2 h-8 w-96" type="text" id="RazorPayId" />
+        </div>
+      <div className="flex flex-col ">
+        <label className="w-fit" htmlFor="RazorPaySecret">RazorPay Secret</label>
+      <input className="bg-[#414248] rounded-lg outline-none p-2 focus:border-blue-900 focus:border-2 h-8 w-96" type="text" id="RazorPaySecret" />
+        </div>
+    </div>
     </div>
   );
 };
