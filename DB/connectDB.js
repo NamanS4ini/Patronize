0b91576151df4e0ad608eb7042a3d0fb93b8d04e
Naman Saini <namansaini464@gmail.com>
2025-01-13 14:21:18 +0530

    Day 7 part 1: Completed the payment backend

--- DB/connectDB.js
+++ DB/connectDB.js
@@ -1,9 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
     try {
-      const conn = await mongoose.connect(`mongodb://localhost:27017/patronize`, {
-        useNewUrlParser: true,
-      });
+      const conn = await mongoose.connect(`mongodb://localhost:27017/patronize`);
       console.log(`MongoDB Connected: {conn.connection.host}`);
     } catch (error) {
       console.error(error.message);
--- app/api/razorpay/route.js
+++ app/api/razorpay/route.js
@@ -5,6 +5,34 @@ import Razorpay from "razorpay";
 import connectDB from "@/DB/connectDB";
 
 export const POST = async (req) => {
-    await connectDB()
-    let body = await req.formData()
-}
+  await connectDB();
+  let body = await req.formData();
+  body = Object.fromEntries(body);
+
+  let p = await payment.findOne({ oid: body.razorpay_order_id });
+
+  if (!p) {
+    return NextResponse.json({succes: false, message:"Oid not found"});
+  }
+
+  let xx = validatePaymentVerification(
+    {
+      "order_id": body.razorpay_order_id,
+      "payment_id": body.razorpay_payment_id,
+    },
+    body.razorpay_signature,
+    process.env.NEXT_PUBLIC_RAZORPAY_SECRET
+  );
+
+  if (xx) {
+    const updatedPayment = await payment.findOneAndUpdate(
+      { oid: body.razorpay_order_id },
+      { done: true },
+      { new: true }
+    );
+    return NextResponse.redirect(`localhost:3000/${updatedPayment.toUser}?paymentDone=true`);
+  }
+  else{
+    return NextResponse.json({succes: false, message:"Payment Verification failed"});
+  }
+};
